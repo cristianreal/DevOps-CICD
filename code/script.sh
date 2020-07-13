30dbfbee6e385d5034969d96cbdc64cf8a9e2e00
cristianreal <201513700@ingenieria.usac.edu.gt>
2020-07-12 18:39:58 -0600

    metodo agregar proveedor

--- code/script.sh
+++
@@ -1,7 +0,0 @@
-docker stop $(docker ps -aq)
-cd /home/elmerreal/DevOps-CI-CD/code/backend
-#docker build -t christianreal/node-server .
-docker run --env-file env.list -p 5000:5000 -d christianreal/node-server
-cd /home/elmerreal/DevOps-CI-CD/code/client
-docker build -t christianreal/vue-client .
-docker run --env-file env.list -p 8086:80 -d christianreal/vue-client
