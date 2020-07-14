8f1d3ae217e3ce1ea3e30d35820c087d3e5d827b
cristianreal <201513700@ingenieria.usac.edu.gt>
2020-07-14 00:04:55 -0600

    Aumentar nodos gke

--- deployment/Terraform/cluster_k8/main.tf
+++ deployment/Terraform/cluster_k8/main.tf
@@ -22,7 +22,7 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
   node_count = 1
   
   autoscaling {
-    max_node_count = 1
+    max_node_count = 2
     min_node_count = 1
   }
   
