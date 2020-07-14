5402cc383a24d006dbce2fd7ba8acd8b6c7cf7e7
cristianreal <201513700@ingenieria.usac.edu.gt>
2020-07-14 15:59:22 -0600

    Pruebas mocha

---
+++ code/backend/server/tests/test-marca.js
@@ -0,0 +1,8 @@
+var assert = require("assert");
+describe("Cadenas",function(){
+    describe("subcadenas", function(){
+        it('deberia retornar una subcadena',function(){
+            assert.equal("prueba","prueba");
+        })
+    })
+})
