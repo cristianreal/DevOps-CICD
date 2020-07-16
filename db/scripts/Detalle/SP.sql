649674a41c275f8b20ab64273ae48dd34900e82b
cristianreal <201513700@ingenieria.usac.edu.gt>
2020-07-16 00:41:52 -0600

    Cambios en store procedures #15

--- db/scripts/Detalle/SP.sql
+++ db/scripts/Detalle/SP.sql
@@ -12,7 +12,7 @@ BEGIN
     ROLLBACK;
     SHOW ERRORS;
 END;
-	SET @movimiento = SELECT pk_movimiento FROM movimiento ORDER BY pk_movimiento DESC LIMIT 1;
+	SET @movimiento = (SELECT pk_movimiento FROM movimiento ORDER BY pk_movimiento DESC LIMIT 1);
 	INSERT INTO detalle (cantidad, total, fk_producto, fk_movimiento) 
 	VALUES (ccantidad, ctotal, cfk_producto, @movimiento);
 END //
