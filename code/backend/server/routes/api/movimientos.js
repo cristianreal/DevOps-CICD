330a3d5042f2ec11462899da904eedeff59d326f
cristianreal <201513700@ingenieria.usac.edu.gt>
2020-07-15 20:08:15 -0600

    Cambios en store procedures #8

--- code/backend/server/routes/api/movimientos.js
+++ code/backend/server/routes/api/movimientos.js
@@ -15,7 +15,7 @@ router.get('/', (req, res) => {
 });
 
 //ADD
-router.post('/', urlencodedParser, (req, res) => {
+router.post('/ingreso', urlencodedParser, (req, res) => {
     if(!req.body.tipo_movimiento){
         res.status(400)
         res.json({
@@ -26,9 +26,27 @@ router.post('/', urlencodedParser, (req, res) => {
         let fecha_movimiento = req.body.fecha_movimiento.replace("\'", "");
         let fk_vendedor = req.body.fk_vendedor;
         let fk_proveedor = req.body.fk_proveedor;
-        connection.query('call Ingresos_Movimiento('+tipo_movimiento+'\''+fecha_movimiento+'\','+fk_vendedor+','+fk_proveedor+')', function (err, rows, fields) {
+        connection.query('call Ingreso_Movimiento('+tipo_movimiento+'\''+fecha_movimiento+'\','+fk_vendedor+','+fk_proveedor+')', function (err, rows, fields) {
             if (err) throw res.send('error: ' + err)
             res.send("Ingreso agregado")
+        });
+    }
+});
+
+//ADD
+router.post('/egreso', urlencodedParser, (req, res) => {
+    if(!req.body.tipo_movimiento){
+        res.status(400)
+        res.json({
+            error: "Bad Data"
+        })
+    }else{
+        let tipo_movimiento = req.body.tipo_movimientos;
+        let fecha_movimiento = req.body.fecha_movimiento.replace("\'", "");
+        let fk_vendedor = req.body.fk_vendedor;
+        connection.query('call Egreso_Movimiento('+tipo_movimiento+'\''+fecha_movimiento+'\','+fk_vendedor+')', function (err, rows, fields) {
+            if (err) throw res.send('error: ' + err)
+            res.send("Egreso agregado")
         });
     }
 });
