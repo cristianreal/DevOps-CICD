25b2c082e5dc801589afa8d6421b2b644264413b
cristianreal <201513700@ingenieria.usac.edu.gt>
2020-07-16 23:15:55 -0600

    Cambios vistas frontend #39

--- code/client/src/components/movimiento/ingreso.vue
+++ code/client/src/components/movimiento/ingreso.vue
@@ -1,37 +1,63 @@
 <template>
 <div class="container">
-    <div class="row">
-
-            <div class="col-lg-9">
-                <div style="overflow-y: scroll; height:460px;">
-                    <table align='center' id="data_table" name="data_table" class="table table-hover">
-                        <tr>
-                        <th>Cantidad</th>
-                        <th>Producto</th>
-                        <th>Precio</th>
-                        <th>Subtotal</th>
-                        <th>Acciones</th>
-                        </tr>
-                        <tr>
-                        <td><input type="number" style="width: 6em" class="form-control" v-model="quantity" id="quantity" onchange="calcularSubtotal()"></td>
-                        <td>
-                            <select class="custom-select" v-model="productId" id="productId" name="productId">
-                                    <option selected>Seleccione un producto</option>
-                                    <option v-for="(product) in products"
-                                    v-bind:item="product" 
-                                    v-bind:index="index" 
-                                    v-bind:key="product.pk_producto"
-                                    v-bind:value="product.pk_producto">{{product.nombre}}</option>
-                            </select>
-                        </td>
-                        <td><input type="number" style="width: 6em" step="0.01" class="form-control"  id="precio" onchange="calcularSubtotal()"></td>
-                        <td><input type="number" style="width: 6em" step="0.01" class="form-control" id="subtotal" disabled></td>
-                        <td><input type="button" class='btn btn-primary' v-on:click="add_row;"  value="Agregar Registro"></td>
-                        </tr>
-                    </table>
-                </div>
-            </div>       
-    </div>
+   <div class="row">
+      <div class="card text-white bg-danger mb-3" style="max-width: 20rem;">
+         <div class="card-header">Movimiento ingreso</div>
+         <div class="card-body">
+            <div class="form-group">
+               <label class="col-form-label" for="date">Fecha</label>
+               <input type="date" class="form-control" v-model="date" placeholder="Default input" id="date" name="date">
+            </div>
+            <div class="form-group">
+               <label for="seller">Vendedor</label>
+               <input type="text" class="form-control" placeholder="Ingrese el nombre" v-model="seller" id="seller" name="seller" :value="seller">
+            </div>
+            <div class="form-group">
+               <label class="col-form-label" for="provider">Proveedor</label>
+               <select class="custom-select" v-model="provider" id="provider" name="provider">
+                  <option selected>Seleccione un prpveedor</option>
+                  <option v-for="(provider, index) in providers"
+                     v-bind:item="provider" 
+                     v-bind:index="index" 
+                     v-bind:key="provider.pk_proveedor"
+                     v-bind:value="provider.pk_proveedor">{{provider.nombre}} {{provider.apellido}}</option>
+               </select>
+            </div>
+            <div class="form-group">
+               <button type="button" class="btn btn-primary block" v-on:click="enviarInformacion">Guardar Lote</button>
+            </div>
+         </div>
+      </div>
+   </div>
+   <div class="row">
+      <div style="overflow-y: scroll; height:460px;">
+         <table align='center' id="data_table" name="data_table" class="table table-hover">
+            <tr>
+               <th>Cantidad</th>
+               <th>Producto</th>
+               <th>Precio</th>
+               <th>Subtotal</th>
+               <th>Acciones</th>
+            </tr>
+            <tr>
+               <td><input type="number" style="width: 6em" class="form-control" v-model="quantity" id="quantity" onchange="calcularSubtotal()"></td>
+               <td>
+                  <select class="custom-select" v-model="productId" id="productId" name="productId">
+                     <option selected>Seleccione un producto</option>
+                     <option v-for="(product) in products"
+                        v-bind:item="product" 
+                        v-bind:index="index" 
+                        v-bind:key="product.pk_producto"
+                        v-bind:value="product.pk_producto">{{product.nombre}}</option>
+                  </select>
+               </td>
+               <td><input type="number" style="width: 6em" step="0.01" class="form-control"  id="precio" onchange="calcularSubtotal()"></td>
+               <td><input type="number" style="width: 6em" step="0.01" class="form-control" id="subtotal" disabled></td>
+               <td><input type="button" class='btn btn-primary' v-on:click="add_row;"  value="Agregar Registro"></td>
+            </tr>
+         </table>
+      </div>
+   </div>
 </div>
 </template>
 <script>
