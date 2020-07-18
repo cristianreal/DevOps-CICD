cc747dda34e962aede53482a4837692252f2f7e8
cristianreal <201513700@ingenieria.usac.edu.gt>
2020-07-17 23:04:55 -0600

    cambios frontend #2

--- db/scripts/Movimiento/SP.sql
+++ db/scripts/Movimiento/SP.sql
@@ -50,7 +50,7 @@ END;
 	SELECT 
 	pk_movimiento, 
 	tipo_movimiento, 
-	fecha_movimiento, 
+	DATE_FORMAT(fecha_movimiento, "%d-%m-%y") as fecha_movimiento,
 	(Select concat(u1.nombre,' ',u1.apellido) from usuario as u1 where u1.pk_usuario=m.fk_vendedor) as vendedor, 
 	(Select concat(u2.nombre,' ',u2.apellido) from usuario as u2 where u2.pk_usuario=m.fk_proveedor) as proveedor,
 	fk_vendedor,
