6cb4c7f887a2bac6b031e552dce806bad2f3ef21
cristianreal <201513700@ingenieria.usac.edu.gt>
2020-07-18 11:28:40 -0600

    cambios frontend #25

--- code/client/src/router/index.js
+++ code/client/src/router/index.js
@@ -35,7 +35,7 @@ import loginComponent from '@/components/login.vue'
 
 Vue.use(Router)
 
-export default new Router({
+let router =  new Router({
     routes: [
         {
             path: '/',
@@ -174,3 +174,37 @@ export default new Router({
         }
     ]
 })
+
+router.beforeEach((to, from, next) => {
+    if(to.matched.some(record => record.meta.requiresAuth)) {
+        if (localStorage.getItem('jwt') == null) {
+            next({
+                path: '/login',
+                params: { nextUrl: to.fullPath }
+            })
+        } else {
+            let user = JSON.parse(localStorage.getItem('user'))
+            if(to.matched.some(record => record.meta.is_admin)) {
+                if(user.is_admin == 1){
+                    next()
+                }
+                else{
+                    next({ name: 'userboard'})
+                }
+            }else {
+                next()
+            }
+        }
+    } else if(to.matched.some(record => record.meta.guest)) {
+        if(localStorage.getItem('jwt') == null){
+            next()
+        }
+        else{
+            next({ name: 'userboard'})
+        }
+    }else {
+        next()
+    }
+})
+
+export default router
