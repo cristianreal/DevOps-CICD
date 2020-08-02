f6d7f486d41ee47f08956ac19751eba8d1e0a1bc
cristianreal <201513700@ingenieria.usac.edu.gt>
2020-08-02 17:39:41 -0600

    se agrega componenete de charts al detalle de productos

--- code/client/src/main.js
+++ code/client/src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueGoogleCharts from 'vue-google-charts'
 
 Vue.config.productionTip = false
 Vue.prototype.$http = "127.0.0.1"
@@ -8,6 +9,7 @@ Vue.prototype.$http = "127.0.0.1"
 Vue.prototype.$port = "5000"
 //Vue.prototype.$http = "35.223.168.202"
 //Vue.prototype.$port = "80"
+Vue.use(VueGoogleCharts)
 
 new Vue({
   render: h => h(App), router
