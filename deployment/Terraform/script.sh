1d44f137a33ed9a284c4d6f4bd0ffaca0f9246e9
cristianreal <201513700@ingenieria.usac.edu.gt>
2020-10-14 17:07:51 -0600

    Cambios en la seguridad del cluster de K8 #7

--- deployment/Terraform/script.sh
+++ deployment/Terraform/script.sh
@@ -12,6 +12,8 @@ terraform version
 curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
-kubectl version --client
-echo "Installe kubectl"
-ls /usr/local/bin/kubectl
+
+#Install HELM
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
