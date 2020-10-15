9416201e6778528986b6e3f8a07e6469442f6968
cristianreal <201513700@ingenieria.usac.edu.gt>
2020-10-14 22:50:33 -0600

    Refactorizacion conexion a la base de datos en el backend

--- code/backend/server/database.js
+++ code/backend/server/database.js
@@ -17,23 +17,9 @@ const mysql = require('mysql');
 // Database Connection for Development
 
 let connection = mysql.createPool({
-  //host: '127.0.0.1',
   host: process.env.DB_HOST,
-  //host: '34.68.157.78',
   user: process.env.DB_USER,
-  //user: 'root',
   password: process.env.DB_PASS,
-  //password: '123456',
   database: process.env.DB_DATABASE
-  //database: 'prueba'
 });
-/*
-  connection.connect(function(err) {
-    if (err) {
-      console.error('Error connecting: ' + err.stack);
-      return;
-    }
-    console.log('Connected as thread id: ' + connection.threadId);
-  });
-*/
-  module.exports = connection;
+module.exports = connection;
